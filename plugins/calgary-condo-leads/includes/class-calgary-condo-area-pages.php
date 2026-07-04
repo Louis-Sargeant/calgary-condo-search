2a0f82046dadf21ba77d7af3b077dcfe3ac972c8
Louis-Sargeant <louis@sargeantrealestate.com>
2026-07-04 08:53:40 -0700

    Add mahogany-condos to area pages
    Add Mahogany condo IDX page override

--- plugins/calgary-condo-leads/includes/class-calgary-condo-area-pages.php
+++ plugins/calgary-condo-leads/includes/class-calgary-condo-area-pages.php
@@ -242,6 +242,7 @@ final class Calgary_Condo_Area_Pages {
         'downtown-calgary-condos'  => '[mrp account_id=67196 listing_def=search-1440045 context=recip perm_attr=tmpl~v2][/mrp]',
         'downtown-condos'          => '[mrp account_id=67196 listing_def=search-1440045 context=recip perm_attr=tmpl~v2][/mrp]',
         'beltline-condos'          => '[mrp account_id=67196 listing_def=search-1439738 context=recip perm_attr=tmpl~v2][/mrp]',
+    'mahogany-condos' => '[mrp account_id=67196 listing_def=search-1440072 context=recip perm_attr=tmpl~v2][/mrp]',
     ];
 
     /**
@@ -257,6 +258,7 @@ final class Calgary_Condo_Area_Pages {
         'downtown-calgary-condos',
         'downtown-condos',
         'beltline-condos',
+    'mahogany-condos',
         'east-village-condos',
         'mission-condos',
         'eau-claire-condos',
